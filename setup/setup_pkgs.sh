ca1a0629789652552282810e14d76a80f77493cb
poseidon-code <pritamhalder05062000@gmail.com>
2022-05-03 15:30:47 +0530

    update: pkgs setup

--- setup/setup_pkgs.sh
+++ setup/setup_pkgs.sh
@@ -2,10 +2,15 @@
 
 printf "(poseidon-code) Installing Required Packages"
 
-doas pacman -Syu --noconfirm unzip tar p7zip unrar wget go npm nodejs jdk-openjdk jre-openjdk python python-pip neovim bat lsd ranger neofetch
+doas pacman -Syu --noconfirm unzip tar p7zip unrar wget go npm nodejs jdk-openjdk jre-openjdk python python-pip \
+neovim bat lsd ranger neofetch starship moc nano firefox base base-devel btop git libdbusmenu-glib vlc telegram-desktop discord libreoffice-fresh \
+latte kde-network-meta kde-system-meta kde-utilities-meta kde-accessibility-meta dolphin-plugins gwenview elisa ark okular spectacle konsole kate dolphin kmail packagekit packagekit-qt5 \
+pipewire pipewire-alsa pipewire-pulse pipewire-jack xdg-desktop-portal xdg-desktop-portal-kde
 doas pacman -Scc
-doas pacman -Rs $(pacman -Qdtq)
+doas pacman -Rsc $(pacman -Qdtq)
 
-yay -Syu --noconfirm visual-studio-code-bin cava mocp peaclock
+yay -Syu --noconfirm visual-studio-code-bin cava peaclock
 yay -Scc
 yay -Rs $(yay -Qdtq)
+
+sudo systemctl enable --now pipewire-pulse.service
