fe03fd371090c6bf64d14aa6dddcac8839aa5549
poseidon-code <pritamhalder05062000@gmail.com>
2022-05-03 14:17:30 +0530

    update: POSIX compliance

--- setup/setup_doas.sh
+++ setup/setup_doas.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/sh
 
 printf "(poseidon-code) Installing doas"
 
@@ -6,4 +6,4 @@ pacman -Syu opendoas
 printf "permit persist :wheel" >> /etc/doas.conf
 chown -c root:root /etc/doas.conf
 chmod -c 0400 /etc/doas.conf
-doas -C /etc/doas.conf && echo "doas config OK" || echo "doas config ERROR"
+doas -C /etc/doas.conf && echo "doas config: OK" || echo "doas config: ERROR"
--- setup/setup_zsh.sh
+++ setup/setup_zsh.sh
@@ -1,13 +1,13 @@
-#!/bin/zsh
+#!/bin/sh
 
 printf "(poseidon-code) Installing zsh"
 
-cp ./.zshrc ./.zshenv ./.zprofile $HOME
-cp ./.zshrc /root/
+cp ../.zshrc ../.zshenv ../.zprofile /home/$(whoami)/.zsh/
+cp ../.zshrc /root/
 
 pacman -Syu zsh zsh-completion zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-match
 
 chsh $(which zsh)
 chsh -s $(which zsh) $(whoami)
 
-. $HOME/.zshrc
+source /home/$(whoami)/.zsh/.zshrc
