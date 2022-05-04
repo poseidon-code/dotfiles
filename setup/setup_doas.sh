c59c7198f5af06427ea1c102f7216ed11b44d38f
poseidon-code <pritamhalder05062000@gmail.com>
2022-05-04 10:49:05 +0530

    fix: zsh & doas setups

--- setup/setup_doas.sh
+++ setup/setup_doas.sh
@@ -3,7 +3,7 @@
 printf "(poseidon-code) Installing doas"
 
 pacman -Syu opendoas
-printf "permit persist :wheel" >> /etc/doas.conf
+echo "permit persist :wheel" > /etc/doas.conf
 chown -c root:root /etc/doas.conf
 chmod -c 0400 /etc/doas.conf
 doas -C /etc/doas.conf && echo "doas config: OK" || echo "doas config: ERROR"
